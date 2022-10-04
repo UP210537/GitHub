28ff4f9d440377eb2b470fcfe963095ddb7267f4
UP210537 <113135605+UP210537@users.noreply.github.com>
2022-10-04 13:40:46 -0500

    U2

--- U2/E03_Tablas.cpp
+++ U2/E03_Tablas.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main (int argc, char const *argv[])
 {
     int n = 5;
+    int r = 0;
     for (int i = 1; i <= 10; i++)
     {
-        int r = n * i;
+        r = n * i;
         cout << n << " x " << i << " = " << r << endl; //endl es para avanzar la línea
     }
     return 0;
