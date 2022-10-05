38bef55c396af81881987cd3704914ec7a617e00
UP210537 <113135605+UP210537@users.noreply.github.com>
2022-10-05 17:23:27 -0500

    U2

--- U2/E03_Tablas.cpp
+++ U2/E03_Tablas.cpp
@@ -5,10 +5,52 @@ int main (int argc, char const *argv[])
 {
     int n = 5;
     int r = 0;
+    int c = 1;
+    cout << "Tabla del cinco con:";
+    cout << "\n" << "For" << "\n";
     for (int i = 1; i <= 10; i++)
     {
         r = n * i;
         cout << n << " x " << i << " = " << r << endl; //endl es para avanzar la línea
     }
+    cout << "\n" << "For invertido" << "\n";
+    for (int i = 10; i >= 1; i --)
+    {
+        r = n * i;
+        cout << n << " x " << i << " = " << r << endl;
+    }
+    cout << "\n" << "While" << "\n";
+    while (c <= 10)
+    {
+        r = n * c;
+        cout << n << " x " << c << " = " << r << endl;
+        c++;
+    }
+    cout << "\n" << "While invertido" << "\n";
+    c = 10;
+    while (c >= 1)
+    {
+        r = n * c;
+        cout << n << " x " << c << " = " << r << endl;
+        c--;
+    }
+    cout << "\n" << "Do While" << "\n";
+    c = 1;
+    do
+    {
+        r = n * c;
+        cout << n << " x " << c << " = " << r << endl;
+        c++;
+    }
+    while (c <= 10);
+    cout << "\n" << "Do While invertido" << "\n";
+    c = 10;
+    do
+    {
+        r = n * c;
+        cout << n << " x " << c << " = " << r << endl;
+        c--;
+    }
+    while (c >= 1);
     return 0;
 }
