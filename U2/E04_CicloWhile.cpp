e2048e86cc7b5ec85ad5d112affaa5d691edb50e
UP210537 <113135605+UP210537@users.noreply.github.com>
2022-10-06 13:14:14 -0500

    U2

---
+++ U2/E04_CicloWhile.cpp
@@ -0,0 +1,31 @@
+//Objetivo: While mientras True
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int i;
+    char opcion = 's';
+    printf ("Tabla de multiplicar \n");
+    while (opcion == 's')
+    {
+        // Entrada
+        printf ("Numero: ");
+        scanf ("%d", &n);
+        
+        // Proceso
+        i = 1;
+        do
+        {
+          printf ("%d x %d = %d \n", n, i, n * i);
+          i++;
+        } while (i <= 10);
+        
+        printf ("\nDesea otro <si/no> ");
+        scanf ("%s", &opcion);
+    }
+    printf ("\n...Hecho");
+    return 0;
+}
