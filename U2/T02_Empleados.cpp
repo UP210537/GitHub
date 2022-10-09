2d15c7c8f94d329667be02ac81f34409752ef026
UP210537 <113135605+UP210537@users.noreply.github.com>
2022-10-09 16:24:00 -0500

    U2

---
+++ U2/T02_Empleados.cpp
@@ -0,0 +1,38 @@
+// Objetivo: Escribir un programa que lea la puntuación del usuario e indique su nivel de rendimiento así como la cantidad de dinero que recibirá el usuario.
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    double p;
+    int dinero = 2400;
+    int r;
+    cout << "Indique su puntuacion: ";
+    cin >> p;
+    if (p == 0.0 || p == 0.4 || p >= 0.6)
+    {
+        if (p == 0.0)
+    {
+        cout << "Inaceptable :(";
+        r = dinero * p;
+        cout << "\nDinero recibido: " << r;
+    }
+    else if (p == 0.4)
+    {
+        cout << "Aceptable";
+        r = dinero * p;
+        cout << "\nDinero recibido: " << r;
+    }
+    else if (p >= 0.6)
+    {
+        cout << "Meritorio :)";
+        r = dinero * p;
+        cout << "\nDinero recibido: " << r;
+    }
+    }
+    else 
+    {
+        cout << "Dato invalido :(";
+    }
+    return 0;
+}
